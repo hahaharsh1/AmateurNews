1fcd402e20b9bb909d995908cd64b475c924a702
Harsh Naik <102807766+hahaharsh1@users.noreply.github.com>
2023-04-27 16:11:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import React, {useState} from 'react'
 import Navbar from './components/Navbar';
+import { Switch } from 'react-router-dom';
 import NewsBody from './components/NewsBody';
 import{BrowserRouter as Router,Switch,Route} from "react-router-dom";
 // import LoadingBar from 'react-top-loading-bar';
